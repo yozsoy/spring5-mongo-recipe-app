cdedddf69faf2378db87bb27e2b4e2591e2abc85
y00562359 <ylozsoy@gmail.com>
2020-04-11 22:43:12 +0300

    322) Integration testing with MongoDB. UOMRepositoryIT test fixed

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryTestIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -1,40 +1,50 @@
 package guru.springframework.repositories;
 
+import guru.springframework.bootstrap.RecipeBootstrap;
 import guru.springframework.domain.UnitOfMeasure;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
-class UnitOfMeasureRepositoryTestIT {
+@DataMongoTest
+public class UnitOfMeasureRepositoryTestIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    CategoryRepository categoryRepository;
 
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() {
+        recipeRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+
+        recipeBootstrap.onApplicationEvent(null);
     }
 
     @Test
-    //@DirtiesContext //creates a different context for the method
-    void findByDescription() {
+    public void findByDescription() {
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
         assertEquals("Teaspoon", uomOptional.get().getDescription());
     }
 
     @Test
-    void findByDescriptionCup() {
+    public void findByDescriptionCup() {
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
         assertEquals("Cup", uomOptional.get().getDescription());
     }
